6130c9aed8a736954dc91014ef8c3c1890e5d834
fahadalduaij <fahad_osama@hotmail.com>
2021-10-04 19:46:38 +0300

    Done Part 1

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,18 +3,25 @@ import ProductItem from "./ProductItem";
 import SearchBar from "./SearchBar";
 // Data
 import products from "../products";
+import { useState } from "react";
 
 const ProductList = () => {
-  const productList = products.map((product) => (
-    <ProductItem product={product} key={product.id} />
-  ));
 
-  return (
-    <>
-      <SearchBar />
-      <div className="listWrapper">{productList}</div>
-    </>
-  );
+
+	const [query, setQuery] = useState("");
+
+  let filtered = products
+		.filter((search) => search.name.includes(query))
+    const productList = filtered.map((product) => (
+      <ProductItem product={product} key={product.id} />
+    ));
+
+	return (
+		<>
+			<SearchBar setQuery={setQuery} />
+			<div className="listWrapper">{productList}</div>
+		</>
+	);
 };
 
 export default ProductList;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,7 +5,7 @@ import React from "react";
 const SearchBar = (props) => {
   return (
     <input className="searchBar"
-      onChange={(event) => console.log(event.target.value)}
+      onChange={(event) => props.setQuery(event.target.value)}
       placeholder="Search for a cookie name"
     />
   );
